b53554b305e70d44b741070c876a14d7ec04e681
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-08-03 01:36:53 +0900

    week_02 3문제 성공 (#11)
    * BOJ_1181 단어 정렬 풀이
    
    * BOJ_15970 화살표 그리기
    
    * BOJ_20291 파일 정리

---
+++ SunKyoung/week_02/BOJ_1181.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+//단어 정렬
+public class BOJ_1181 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		String[] str = new String[N];
+		
+		//단어 저장
+		for(int i=0; i<N; i++) {
+			str[i] = br.readLine();
+		}
+		
+		//Arrays.sort() 정렬조건 재정의
+		Arrays.sort(str, (String o1, String o2)->o1.length()==o2.length()?o1.compareTo(o2):o1.length()-o2.length());
+		
+		//0번째 문자는 무조건 처음 등장한 단어
+		sb.append(str[0]).append("\n");
+		//중복단어 제거
+		for(int i=1; i<N; i++) {
+			//직전의 문자열과 현재 문자열이 다르다면 출력 대상
+			if(!str[i].equals(str[i-1]))
+				sb.append(str[i]).append("\n");
+		}
+		
+		System.out.print(sb);
+		
+	}
+}
