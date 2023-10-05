22dbde8afc35ea7542e4d8661c4fe03828e1f2e5
seojeong <sjytis14@naver.com>
2023-10-06 00:10:27 +0900

    BOJ_2252 줄세우기

---
+++ Seojeong/week_05/BOJ_2252.java
@@ -0,0 +1,55 @@
+package Seojeong.week_05;
+
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BOJ_2252 {
+    static int N, M;
+    static Queue<Integer> queue;
+    static ArrayList<Integer>[] adj;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+        adj = new ArrayList[N + 1];
+        queue = new ArrayDeque<>();
+        int[] degree = new int[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            adj[A].add(B);
+            degree[B]++;
+        }
+
+
+        while (true) {
+            for (int i = 1; i <= N; i++) {
+                if (degree[i] == 0) {
+                    degree[i] = -1;
+                    queue.offer(i);
+                    break;
+                }
+            }
+
+            if (queue.isEmpty()) break;
+
+            int v = queue.poll();
+            for (Integer vertex : adj[v]) {
+                degree[vertex] -= 1;
+            }
+            System.out.print(v + " ");
+        }
+
+
+    }
+}
